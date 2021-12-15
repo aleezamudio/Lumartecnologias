13d6cab05a4f621d96eb79a4431c93d46c437df7
Zamudio Alejandro <ale-zamudio2011@hotmail.com>
2021-12-14 23:00:55 -0300

    ItemDetailContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React , {Fragment} from 'react';
-import NavBar from './components/NavBar';
+import React , {Fragment} from 'react'
+import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 import ItemCount from './components/ItemCount'
+import ItemDetailContainer from './components/ItemDetailContainer'
+import ItemDetail from './components/ItemDetail'
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
      <ItemListContainer presentacion="PROOVEDOR,Ventas por mayor y por menor,Consultas mediante nuestras redes sociales,Precios accesibles y promociones dentro del mercado de Pc, Electrodomesticos, Artefactos electronicos, etc." />
    
    <ItemCount className="container"/>
+   <ItemDetailContainer/>
+  
    </Fragment>
   );
 }
